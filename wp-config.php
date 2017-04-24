b1f65265f95cb426a36c83add639fd86728346e7
Karen Pokhikyan <pokhikyan@gmail.com>
2017-04-24 14:11:39 +0400

    Second commit

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'kristina');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'mysqly');
+define('DB_PASSWORD', 'mysql');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
