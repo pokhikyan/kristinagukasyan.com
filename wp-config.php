8ef2a7b400f2ecb95e6fe8a66f0583064af05551
Karen Pokhikyan <pokhikyan@gmail.com>
2017-04-24 14:04:28 +0400

    second

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'kristina');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'mysqlyy');
+define('DB_PASSWORD', 'mysqly');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
